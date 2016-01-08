12b289ce2213b3b6fc3db61e5a49690b80ed80c6
jmokrzycki <jarek.mokrzycki@gmail.com>
2016-01-08 16:29:54 +0100

    dziala strona

--- scripts/buildRedeploy.sh
+++ scripts/buildRedeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "************ UNDEPLOYING *******************"
-./../tje2/scripts/glassfish3/glassfish/bin/asadmin undeploy bazaFilmow
+./scripts/glassfish3/glassfish/bin/asadmin undeploy bazaFilmow
 echo "************ BUILDING **********************"
 mvn package
 echo "************ DEPLOYING *********************"
-./../tje2/scripts/glassfish3/glassfish/bin/asadmin deploy target/bazaFilmow.war
+./scripts/glassfish3/glassfish/bin/asadmin deploy target/bazaFilmow.war
